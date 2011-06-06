ad7c6abd456aeaa736927e30bba114f273e9d7bd
JK Wood <joshuakwood@gmail.com>
2011-06-06 06:01:00 -0500

    Built create method for projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,11 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  
+  def create
+    @project = Project.new(params[:project])
+    @project.save
+    flash[:notice] = "Project has been created"
+    redirect_to @project
+  end
 end
